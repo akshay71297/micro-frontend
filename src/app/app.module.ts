5bc3db00ee98cdcac045e151fa0963e0bacd3977
AK <akshaykumar71297@gmail.com>
2022-03-29 23:36:43 -0300

    updated to support module federation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {RouterModule, Routes} from "@angular/router";
+const routes: Routes = [
+  { path: '', component: AppComponent }
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forChild(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
